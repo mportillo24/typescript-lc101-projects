1515fa48aa90963176978e1c6939022723d8e66e
Mario Portillo <marioportillo@Marios-MacBook-Pro.local>
2019-12-09 13:24:32 -0600

    Completed part 3 of the exercise.

--- exercises/part3.ts
+++ exercises/part3.ts
@@ -8,9 +8,15 @@ let kilometersToTheMoon: number = 384400;
 let milesPerKilometer: number = 0.621;
 
 // Code the "getDaysToLocation" function here:
-
-
+let getDaysToLocation = function(kilometersAway: number): number {
+    let milesAway: number = (kilometersAway * milesPerKilometer);
+    let hours: number = (milesAway / speedMph);
+    let daysToLocation: number = (hours / 24);
+    return daysToLocation;
+}
 
 
 
 // Call the function and print the outputs for the Mars trip and the moon trip:
+console.log(`${spacecraftName} would take ${getDaysToLocation(kilometersToMars)} days to get to Mars.`);
+console.log(`${spacecraftName} would take ${getDaysToLocation(kilometersToTheMoon)} days to get to the Moon.`);
